afde87809e27610308101388501909ce23c70fd9
anoopreddykunta <anoopreddy240@gmail.com>
2023-11-14 03:05:15 -0800

    Making resolution changes

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -19,25 +19,20 @@ export const About = () => {
             <BsFillAwardFill className='about__icon'/>
               <h5>Internship - Broadridge Financial Solutiona </h5> 
               <small> Internship </small>
-              <p>• Crafted reusable Java code to enhance our projects' quality and scalability.
-                 • Strengthened security by addressing vulnerabilities and ensuring robust defences.
-                 • Streamlined data loading through optimized PL/SQL procedures, ensuring accuracy and efficiency. 
-                 • Led software upgrades aligned with business needs, resolving issues for enhanced functionality.</p>
+              <p>• Crafted reusable Java code to enhance our projects' quality and scalability. </p>
+              <p>  • Strengthened security by addressing vulnerabilities and ensuring robust defences.  </p>
+              <p>  • Streamlined data loading through optimized PL/SQL procedures, ensuring accuracy and efficiency.  </p>
+              <p>  • Led software upgrades aligned with business needs, resolving issues for enhanced functionality.</p>
             </article>
             <article className='about__card'>
             <BsFillAwardFill className='about__icon'/>
               <h5>Full Time - Broadridge Financial Solution </h5> 
               <small> Junior Software Engineer </small>
-              <p>
-              • Assisted in migrating codebase and functionality from EJB to Spring, resulting in improved application structure and maintainability.
-              • Collaborated on rewriting frontend components from JSP to ReactJS, enhancing user experience and enabling modern UI features.
-              • Designed and implemented APIs that retrieve data from backend systems, enabling seamless integration with the frontend ensuring up-to-date information presentation.
-              • These APIs helped in improving data retrieval efficiency and reducing latency, resulting in a smoother user experience.
-              • Assumed responsibility for coordinating and overseeing the QA database, ensuring its proper functioning and
-                accessibility.
-              • Made necessary changes to PL/SQL procedures, improving data manipulation and retrieval capabilities.
-              • Helped in estimating and resolving the security vulnerabilities and rectifying cross-site scripting vulnerabilities
-                across the application.
+              <p>• Assisted in migrating codebase and functionality from EJB to Spring, resulting in improved application structure and maintainability.</p>
+           <p>   • Collaborated on rewriting frontend components from JSP to ReactJS, enhancing user experience and enabling modern UI features. </p>
+            <p>  • Designed and implemented APIs that retrieve data from backend systems, enabling seamless integration with the frontend ensuring up-to-date information presentation. </p>
+            <p> • These APIs helped in improving data retrieval efficiency and reducing latency, resulting in a smoother user experience.
+             
               </p>
             </article>
 
@@ -45,18 +40,17 @@ export const About = () => {
             <IoSchoolSharp className='about__icon'/>
               <h5>College </h5> 
               <small> Master's in Computer Science <br />
-                      California State University - Dominguez Hills
+                     
               </small>
-              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti reiciendis non nisi. Atque tenetur aperiam laboriosam magni! Accusamus doloribus aperiam, fuga quibusdam quisquam dicta voluptate, sapiente earum et soluta vero?</p>
-            </article>
-            <article className='about__card'>
-            <PiFoldersDuotone className='about__icon'/>
-              <h5>Projects </h5> 
-              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod, repudiandae temporibus ex vel, dolorem molestias quibusdam beatae vitae tempora ullam officia tenetur! Commodi neque voluptate sint modi ipsam in et?</p>
+              <p> California State University - Dominguez Hills</p>
+              <p>• Fall 2023 Semester</p>
+              <p>  </p>
+              <p>  </p>
+              <p>  </p>
             </article>
            </div>
 
-           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit laudantium facere in iste explicabo neque illo. Neque assumenda repellendus architecto omnis, animi eius! Ullam earum recusandae officiis, exercitationem magnam ducimus?</p>
+           <p></p>
           <a href="#contact" className='btn btn-primary'>Let's Talk</a>
       </div>
     </div>
