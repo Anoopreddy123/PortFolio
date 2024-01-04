23cb85df4a002ecc867e7b1d623fcf8de61bc7fc
anoopreddykunta <anoopreddy240@gmail.com>
2024-01-03 20:57:37 -0800

    Updated resume

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -19,21 +19,22 @@ export const About = () => {
             <BsFillAwardFill className='about__icon'/>
               <h5>Internship - Broadridge Financial Solutiona </h5> 
               <small> Internship </small>
-              <p>• Crafted reusable Java code to enhance our projects' quality and scalability. </p>
-              <p>  • Strengthened security by addressing vulnerabilities and ensuring robust defences.  </p>
-              <p>  • Streamlined data loading through optimized PL/SQL procedures, ensuring accuracy and efficiency.  </p>
-              <p>  • Led software upgrades aligned with business needs, resolving issues for enhanced functionality.</p>
+              <p>  • Played a pivotal role in creating sophisticated, reusable Java code with a deep understanding of Object-Oriented Programming, contributed to code rewriting, leveraging cutting-edge Java features, leading to enhancements in project quality and scalability.</p>
+              <p>  • Enhanced security measures by addressing vulnerabilities, resulting in a 25% reduction in potential threats and reinforcing robust defensesacross the system.</p>
+              <p>  • Leveraged in-depth proficiency in batch processing, incorporating a nuanced understanding of SQL Loader, to streamline data loading.Utilized PL/SQL procedures, ensuring heightened accuracy and efficiency for data management system.</p>
+              <p>  • Facilitated impactful software upgrades aligned with business needs, resulting in a 35% improvement in system functionality, resolving issues and optimizing overall performance.</p>
             </article>
             <article className='about__card'>
             <BsFillAwardFill className='about__icon'/>
               <h5>Full Time - Broadridge Financial Solution </h5> 
               <small> Junior Software Engineer </small>
-              <p>• Assisted in migrating codebase and functionality from EJB to Spring, resulting in improved application structure and maintainability.</p>
-           <p>   • Collaborated on rewriting frontend components from JSP to ReactJS, enhancing user experience and enabling modern UI features. </p>
-            <p>  • Designed and implemented APIs that retrieve data from backend systems, enabling seamless integration with the frontend ensuring up-to-date information presentation. </p>
-            <p> • These APIs helped in improving data retrieval efficiency and reducing latency, resulting in a smoother user experience.
-             
-              </p>
+              <p> • Conceptualized and executed the migration from EJB to Spring, resulting in a 30% transformation in application structure and improved maintainability.</p>
+              <p> • Collaborated on rewriting frontend components from JSP to ReactJS, resulting in a 40% improvement in user experience and the seamless incorporation of modern UI features. </p>
+              <p> • Designed and orchestrated APIs for seamless data retrieval from backend systems, ensuring fluid integration with the frontend and achieving a 25% enhancement in data retrieval efficiency metrics for up-to-date information presentation.</p>
+              <p> • Implemented APIs optimizing data retrieval, achieving a 20% reduction in latency, ultimately enhancing the user experience with a noticeably smoother data presentation.</p>
+              <p> • Facilitated the management of the QA database, identifying and rectifying data discrepancies by executing SQL procedures, thereby minimizing the impact.</p>
+              <p> • Led the internship program as the primary contact, guiding interns in tasks by providing hands-on assistance and fostering a clear understanding, contributing to a successful and enriching learning experience.</p>
+              <p>  • Co-ordinated in identifying and addressing security vulnerabilities, rectifying cross-site scripting issues across the application and achieving a notable 30% improvement in overall security.</p>
             </article>
 
             <article className='about__card'>
